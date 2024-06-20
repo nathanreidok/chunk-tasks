419ca3497e1433de1090660550723ed0f8f894c3
Nathan Reid <nathanreidok@gmail.com>
2024-06-20 14:15:13 -0500

    Update example to chunktasks

--- src/main/java/com/chunktasks/ChunkTasksConfig.java
+++ src/main/java/com/chunktasks/ChunkTasksConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup("chunktasks")
 public interface ChunkTasksConfig extends Config
 {
 	String CONFIG_GROUP = "chunk-tasks";
